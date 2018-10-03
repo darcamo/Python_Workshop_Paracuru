ea113cc2750858864b087b3d224e850dd37bab0a
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-10-03 20:05:57 -0300

    changes

---
+++ slides/python/code/unittest_basic.py
@@ -0,0 +1,15 @@
+import unittest
+
+# Ao invés de definir fun aqui, nós também podemos importá-la de outro módulo
+def fun(x):
+    return x + 1
+
+
+class MyTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(fun(3), 4)
+
+
+if __name__ == '__main__':
+    # Automaticamente executa todos os métodos com nome "test_alguma_coisa" será executado
+    unittest.main()
